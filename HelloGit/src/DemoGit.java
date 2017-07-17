92584980b0206cb57db14e2067eb77443c92bd43
Prasad <reddiprasad.m@gmail.com>
2017-07-18 00:32:35 +0530

    add more message

--- HelloGit/src/DemoGit.java
+++ HelloGit/src/DemoGit.java
@@ -4,7 +4,7 @@ public class DemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//welocme to demo
-		System.out.println("Hai Git Demo");
+		System.out.println("Hai Git Demo to work on this");
 
 	}
 
