8978ad2ee15c858403468b6b5dab344ab6025fd8
mrprasad <reddiprasad.m@gmail.com>
2017-07-26 19:46:56 +0530

    Update DemoGit.java
    df

--- HelloGit/src/DemoGit.java
+++ HelloGit/src/DemoGit.java
@@ -4,7 +4,7 @@ public class DemoGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//welocme to demo
-		//for today fromoff
+		//for today fromoffff
 		System.out.println("Hai Git Demo to work on this");
 
 	}
